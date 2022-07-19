724aeba36437cd5a5deff83bf77de7f61e30b0c2
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-07-19 15:14:42 -0500

    Componente CheckPrice Completo

--- app/Http/Livewire/CheckPrice.php
+++ app/Http/Livewire/CheckPrice.php
@@ -2,12 +2,197 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Token;
+use Carbon\Carbon;
+use Exception;
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class CheckPrice extends Component
 {
+    public $response, $rapi, $fecha_actual, $expiry_time, $token, $responseCotizacion, $switchCotizacion = false;
+    public $nombre_1, $nombre_2, $apellido_1, $apellido_2, $fecha_nacimiento, $paquete, $plan, $inicio_viaje, $fin_viaje;
+    public $fFechaNacimiento, $fInicioViaje, $fFinViaje;
+
+    protected $rules = [
+        'nombre_1' => 'required|max:50|regex:/^([a-zA-ZùÙüÜäàáëèéïìíöòóüùúÄÀÁËÈÉÏÌÍÖÒÓÜÚñÑ\s]+)$/',
+        'nombre_2' => 'max:50',
+        'apellido_1' => 'required|max:50|regex:/^([a-zA-ZùÙüÜäàáëèéïìíöòóüùúÄÀÁËÈÉÏÌÍÖÒÓÜÚñÑ\s]+)$/',
+        'apellido_2' => 'max:50',
+        'fecha_nacimiento' => 'required',
+        'paquete' => 'required',
+        'plan' => 'required',
+        'inicio_viaje' => 'required',
+        'fin_viaje' => 'required',
+    ];
+
+    protected $messages = [
+        'nombre_1.required' => 'Este campo es obligatorio',
+        'nombre_1.max' => 'Máximo 50 caracteres',
+        'nombre_1.regex' => 'Este campo solo debe contener letras y espacios',
+        'nombre_2.max' => 'Máximo 50 caracteres',
+        'apellido_1.required' => 'Este campo es obligatorio',
+        'apellido_1.max' => 'Máximo 50 caracteres',
+        'apellido_2.regex' => 'Este campo solo debe contener letras y espacios',
+        'apellido_2.max' => 'Máximo 50 caracteres',
+        'fecha_nacimiento.required' => 'Esta campo es obligatorio',
+        'paquete.required' => 'Esta campo es obligatorio',
+        'plan.required' => 'Esta campo es obligatorio',
+        'inicio_viaje.required' => 'Esta campo es obligatorio',
+        'fin_viaje.required' => 'Esta campo es obligatorio',
+    ];
+
+
     public function render()
     {
         return view('livewire.check-price');
+    }
+
+    public function checkToken()
+    {
+        $this->response = Token::latest()->first();
+        $this->response != null ? $this->checkExpiryTime() : $this->createToken();
+    }
+
+    public function createToken()
+    {
+        $this->rapi = Http::asForm()->post('https://apiintegracion.ins-cr.com/v1/connect/token', [
+            'grant_type' => 'client_credentials',
+            'client_id' => '3101571319',
+            'client_secret' => '4ea792b2-a70f-ce74-9646-b07a823ab669',
+            'allowed_scopes' => '',
+        ]);
+
+        $this->response = $this->rapi->collect($key = null)->all();
+        $this->token = $this->response['access_token'];
+        $this->expiry_time = Carbon::now()->addSeconds(3600);
+        $this->uploadTokenToDb($this->response, $this->expiry_time);
+    }
+
+    public function checkExpiryTime()
+    {
+        $this->fecha_actual = Carbon::now();
+        if ($this->response->expiry_time < $this->fecha_actual) {
+            $this->response->delete();
+            $this->createToken();
+        } else {
+            $this->token = $this->response->access_token;
+            $this->checkPrice();
+        }
+    }
+
+    public function uploadTokenToDb($response, $expiry_time)
+    {
+        Token::create([
+            'access_token' => $response['access_token'],
+            'expires_in' => $response['expires_in'],
+            'expiry_time' => $expiry_time,
+            'token_type' => $response['token_type'],
+            'scope' => $response['scope']
+        ]);
+        $this->checkToken();
+    }
+
+    public function checkPrice()
+    {
+        $this->validate();
+
+        try {
+            $this->formatDateData();
+
+            $this->responseCotizacion = Http::withToken($this->token)
+                ->acceptJson()
+                ->post('https://apiintegracion.ins-cr.com/v1/polizas/cotizacion', [
+                    "consecutivoConfiguracion" => $this->paquete,
+                    "parametros" => [
+                        "TIPOIDCLI" => "",
+                        "IDENTCLI" => "",
+                        "PRIAPELLIDOCLI" => $this->apellido_1,
+                        "SEGAPELLIDOCLI" => $this->apellido_2,
+                        "PRINOMBRECLI" => $this->nombre_1,
+                        "SEGNOMBRECLI" => $this->nombre_2,
+                        "NOMCOMPLETOCLI" => "",
+                        "GENEROCLI" => "",
+                        "FECNACICLI" => $this->fFechaNacimiento,
+                        "TIPOTELCLI" => "",
+                        "NUMTELCLI" => "",
+                        "PROVCLI" => "",
+                        "CANTCLI" => "",
+                        "DISTCLI" => "",
+                        "CORREOE" => "",
+                        "VIGDESDE" => $this->fInicioViaje,
+                        "VIGHASTA" => $this->fFinViaje,
+                        "NUMPOLIZA" => "",
+                        "SUCEMI" => "18",
+                        "AGENTE" => "6600100",
+                        "FORMAPAGO" => "",
+                        "DIRECRSGO" => "",
+                        "MONTO2" => "",
+                        "NUMOPERACION" => "",
+                        "OBSERVACIONES" => "",
+                        "MONTO1" => "",
+                        "CBEDAD" => "34",
+                        "TIPOACTRSGO" => "0010",
+                        "MOASR" => "",
+                        "TIPORIESGOCOB" => "01",
+                        "TIPOTARIFACOB" => "0126",
+                        "SUMASEGRSGO" => "",
+                        "PHOSINBUC" => "",
+                        "DIRECSINBUC" => "",
+                        "PROVSINBUC" => "",
+                        "DIASVIAJE" => "7",
+                        "CLASERSGO" => $this->plan,
+                        "TIPOIDBENEF1" => "",
+                        "IDBENEF1" => "",
+                        "NOMCOMPBENEF1" => "",
+                        "PARENTESCOBENEF1" => "",
+                        "PORCENTAJEBENEF1" => "",
+                        "NOMBENEF1" => "",
+                        "PRIAPEBENEF1" => "",
+                        "SEGAPEBENEF1" => "",
+                        "TIPOIDBENEF2" => "",
+                        "IDBENEF2" => "",
+                        "NOMCOMPBENEF2" => "",
+                        "PARENTESCOBENEF2" => "",
+                        "PORCENTAJEBENEF2" => "",
+                        "NOMBENEF2" => "",
+                        "PRIAPEBENEF2" => "",
+                        "SEGAPEBENEF2" => "",
+                        "TIPOIDBENEF3" => "",
+                        "IDBENEF3" => "",
+                        "NOMCOMPBENEF3" => "",
+                        "PARENTESCOBENEF3" => "",
+                        "PORCENTAJEBENEF3" => "",
+                        "NOMBENEF3" => "",
+                        "PRIAPEBENEF3" => "",
+                        "SEGAPEBENEF3" => "",
+                        "TIPOIDBENEF4" => "",
+                        "IDBENEF4" => "",
+                        "NOMCOMPBENEF4" => "",
+                        "PARENTESCOBENEF4" => "",
+                        "PORCENTAJEBENEF4" => "",
+                        "NOMBENEF4" => "",
+                        "PRIAPEBENEF4" => "",
+                        "SEGAPEBENEF4" => ""
+                    ]
+                ]);
+            $this->switchCotizacion = true;
+            $this->responseCotizacion = $this->responseCotizacion->collect($key = null)->all();
+        } catch (Exception $ex) {
+            dd($ex);
+        }
+    }
+
+    public function formatDateData()
+    {
+        $this->fFechaNacimiento = Carbon::parse($this->fecha_nacimiento)->format('d/m/Y');
+        $this->fInicioViaje = Carbon::parse($this->inicio_viaje)->format('d/m/Y');
+        $this->fFinViaje = Carbon::parse($this->fin_viaje)->format('d/m/Y');
+        $this->nombre_2 == null ? $this->nombre_2 = '' : '';
+        $this->apellido_2 == null ? $this->apellido_2 = '' : '';
+    }
+
+    public function backToForm(){
+        $this->switchCotizacion = false;
     }
 }
--- resources/views/livewire/check-price.blade.php
+++ resources/views/livewire/check-price.blade.php
@@ -1,3 +1,197 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <div class="py-4 px-6 @if($switchCotizacion == true) hidden @else @endif">
+        <form wire:submit.prevent="checkToken" class="space-y-8 divide-y divide-gray-200">
+            <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+
+                <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+                    <div>
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">Formulario de cotización</h3>
+                    </div>
+                    <div class="space-y-6 sm:space-y-5">
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="country" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Paquete
+                            </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <select wire:model.lazy="paquete" id="country" name="country"
+                                    autocomplete="country-name"
+                                    class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                    <option></option>
+                                    <option value="12111">Viajero con asistencia (salida)</option>
+                                </select>
+                                @error('paquete') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="country" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Plan
+                            </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <select wire:model.lazy="plan" id="country" name="country" autocomplete="country-name"
+                                    class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                    <option></option>
+                                    <option value="0000">Básico</option>
+                                    <option value="0010">Plus</option>
+                                </select>
+                                @error('plan') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="street-address"
+                                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Inicio del viaje </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="inicio_viaje" type="date" name="street-address"
+                                    id="street-address" autocomplete="street-address"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('inicio_viaje') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="street-address"
+                                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Fin del viaje </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="fin_viaje" type="date" name="street-address" id="street-address"
+                                    autocomplete="street-address"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('fin_viaje') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="first-name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Primer
+                                nombre </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="nombre_1" type="text" name="first-name" id="first-name"
+                                    autocomplete="given-name"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('nombre_1') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="first-name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Segundo nombre</label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="nombre_2" type="text" name="first-name" id="first-name"
+                                    autocomplete="given-name"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('nombre_2') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="last-name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Primer
+                                apellido </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="apellido_1" type="text" name="last-name" id="last-name"
+                                    autocomplete="family-name"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('apellido_1') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="last-name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Segundo
+                                apellido </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="apellido_2" type="text" name="last-name" id="last-name"
+                                    autocomplete="family-name"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('apellido_2') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                            <label for="street-address"
+                                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                Fecha de nacimiento </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <input wire:model.lazy="fecha_nacimiento" type="date" name="street-address"
+                                    id="street-address" autocomplete="street-address"
+                                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                @error('fecha_nacimiento') <p class="mt-1 mb-1 text-xs text-red-600 italic">
+                                    {{ $message }}
+                                </p> @enderror
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="pt-5">
+                <div class="flex justify-end">
+                    <button type="submit"
+                        class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cotizar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="@if($switchCotizacion != true) hidden @else flex @endif flex-col w-full text-center mt-4">
+        <h2 class="text-2xl font-bold text-gray-800">Cotización</h2>
+        <div class="text-left">
+            @if ($switchCotizacion == true)
+            <p class="mt-2">Número de cotización: {{ $responseCotizacion['numeroCotizacion'] }}</p>
+            <p class="mt-2">Moneda: {{ $responseCotizacion['descripcionMoneda'] }}</p>
+            <p class="mt-2">Monto anual: {{ $responseCotizacion['primas']['0']['montoPrima'] }}</p>
+            @endif
+        </div>
+
+        <div class="mt-4">
+            <div class="mt-4">
+                <h3 class="text-xl font-bold text-gray-800">Coberturas</h3>
+            </div>
+            <div class="grid grid-cols-3 gap-4">
+                @if ($switchCotizacion == true)
+                @foreach ($responseCotizacion['coberturas'] as $cobertura)
+                <div class="col-span-1 p-4 drop-shadow-lg rounded bg-blue-300">
+                    <div class="flex flex-col items-start">
+                        <p>Descripción: {{ $cobertura['descripcion'] }}</p>
+                        <p>Monto asegurado: {{ $cobertura['montoAsegurado'] }}</p>
+                        <p>Prima: {{ $cobertura['prima'] }}</p>
+                    </div>
+                </div>
+                @endforeach
+                @endif
+            </div>
+            <div class="flex justify-end mt-4">
+                <button wire:click="backToForm" type="button"
+                    class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Regresar</button>
+            </div>
+        </div>
+
+    </div>
 </div>
